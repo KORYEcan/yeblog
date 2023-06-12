7254de5252f390873fadd664ca5d099967425cd5
KORYEcan <philo0401@naver.com>
2023-06-13 02:46:41 +0900

    View 연결완ë료

--- src/main/java/shop/yeblog/config/SecurityConfig.java
+++ src/main/java/shop/yeblog/config/SecurityConfig.java
@@ -1,2 +1,48 @@
-package shop.yeblog.config;public class SecurityConfig {
+package shop.yeblog.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Slf4j
+@Configuration
+public class SecurityConfig {
+
+
+  @Bean
+  BCryptPasswordEncoder passwordEncoder(){    //Ioc에서 관리하는 식으로 사용하기
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    //1. CSRF 해제  --> postman을 사용못하기 떄문에 마지막에 필요하면 이코드를 지우면 됨
+   http.csrf().disable();
+
+   //2.Form 로그인 설정
+    http.formLogin()
+        .loginPage("/loginForm")
+        .successHandler(((request, response, authentication) -> {
+         log.debug("디버그 : 로그인 성공 ");
+        }))
+        .failureHandler(((request, response, exception) -> {
+          log.debug("디버그 : 로그인 실패 :  "+exception.getMessage());
+
+        }));
+    //3. 인증, 권한 필터 설정
+   http.authorizeRequests(authorize -> authorize.antMatchers("/s/**").authenticated() ///주소가  s로 시작하면 로그인안될시 접속불가
+       .anyRequest().permitAll()
+   );
+
+
+
+    return http.build();
+
+  }
+
 }
---
+++ src/main/webapp/WEB-INF/views/layout/footer.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<div class="mt-4 p-5 bg-dark text-white rounded text-center" style="margin-bottom: 0">
+    <p>Created by Yeit</p>
+    <p>📞 010-1234-6789</p>
+    <p>🏴 서울특별시 성동구 XX동</p>
+</div>
+</body>
+
+</html>
