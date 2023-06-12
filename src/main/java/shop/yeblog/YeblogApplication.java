2053697c3a328374d7148a081240790eba6593a9
KORYEcan <philo0401@naver.com>
2023-06-12 21:35:04 +0900

    initial  commit

---
+++ src/main/java/shop/yeblog/YeblogApplication.java
@@ -0,0 +1,13 @@
+package shop.yeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeblogApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(YeblogApplication.class, args);
+  }
+
+}
---
+++ src/test/java/shop/yeblog/YeblogApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.yeblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeblogApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
